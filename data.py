ca64fb4f6d7659ede4b5d7331c55cb5a18931f22
Mutukukioko <95979150+Mutukukioko@users.noreply.github.com>
2022-11-30 15:13:14 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,23 @@
+# Pandas library is used for handling tabular data
+import pandas as pd
+
+# NumPy is used for handling numerical series operations (addition, multiplication, and ...)
+
+import numpy as np
+# Sklearn library contains all the machine learning packages we need to digest and extract patterns from the data
+from sklearn import linear_model, model_selection, metrics
+from sklearn.model_selection import train_test_split
+
+# Machine learning libraries used to build a decision tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+# Sklearn's preprocessing library is used for processing and cleaning the data 
+from sklearn import preprocessing
+
+# for visualizing the tree
+import pydotplus
+from IPython.display import Image
+
+launch_data = pd.read_excel('RocketLaunchDataCompleted.xlsx')
+launch_data.head()
